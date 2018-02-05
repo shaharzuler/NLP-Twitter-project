0215030f9a9f4812d1c5dda33c8768ebc13e17b5
shaharzuler <shahar.zuler@gmail.com>
2018-02-05 19:25:21 +0200

    Update preprocessing function.py

--- preprocessing function.py
+++ preprocessing function.py
@@ -1,4 +1,4 @@
-def review_to_words( raw_review ):
+def tweet_to_words( raw_review ):
     '''preproceccing tweets'''
     # Remove non-letters        
     letters_only = re.sub("[^a-zA-Z]", " ", raw_review) 
@@ -14,6 +14,5 @@ def review_to_words( raw_review ):
     # lemmitizer
     wordnet_lemmatizer = WordNetLemmatizer()
     lammatized_words = [wordnet_lemmatizer.lemmatize(word) for word in  stemmed_words  ]
-    # Join the words back into one string separated by space, 
-    # and return the result.
+    # Join the words back into one string separated by space and return the result.
     return( " ".join( meaningful_words ))   
